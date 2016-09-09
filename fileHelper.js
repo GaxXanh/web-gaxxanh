d4f6102fa8114397ef7fa17071a3c7773e9de059
Pham Hoai Anh <anhph5@fsoft.com.vn>
2016-09-10 00:30:00 +0700

    Done write file

--- fileHelper.js
+++ fileHelper.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const readline = require('readline');
 
 module.exports = {
-    readFile
+    readFile,
+    writeFile
 }
 
 function readFile(filePath, callback) {
@@ -20,4 +21,38 @@ function readFile(filePath, callback) {
     rl.on('close', () => {
         callback(result);
     })
+}
+
+function writeFile(array, option, filePath, callback) {
+
+    var fullText = "";
+
+    for (var text of array) {
+        fullText = fullText.concat(text);
+        fullText += "\n"
+    }
+
+    if (option == "n") {
+        fs.writeFile(fileName, fullText, (err) => {
+            if (err) {
+                throw err;
+                callback(0);
+            }
+            console.log("It\'s save");
+            callback(1);
+        })
+    } else if (option == "a") {
+        fs.readFile(filePath, (err, data) => {
+            fullText = data.toString() + fullText;
+            fs.writeFile(filePath, fullText, (err) => {
+                if (err) {
+                    throw err;
+                    callback(0);
+                }
+                console.log("It\'s save");
+                callback(1);
+            })
+        })
+    }
+
 }
